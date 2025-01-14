565438db2d13daf470d6e9ca182b748466a95369
gabriel-h-nx <gabriel@nxsquad.com.br>
2025-01-14 11:31:06 -0300

    addying header

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styles from "./app.module.css"
+
+import { Header } from "./components/Header";
+
+export function App() {
+  return (
+    <div className={styles.container}>
+      <main>
+      <Header />
+      </main>
+    </div>
+  )
+}
